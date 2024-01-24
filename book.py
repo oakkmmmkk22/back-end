0f7d680ee9091f3114a8fc0605e7d9b9b779b797
rungwikrai <oakkmmmkk22@gmail.com>
2024-01-24 21:27:18 +0700

    report student from _id

--- book.py
+++ book.py
@@ -15,33 +15,42 @@ db = client["students"]
 collection = db["std_info"]
 all_students = collection.find()
 
-stdd = []
+std_all = []
 books=[
     {"id":1,"title":"Book 1","author":"Author 1"},
     {"id":2,"title":"Book 2","author":"Author 2"},
     {"id":3,"title":"Book 3","author":"Author 3"}
 ]
+for std in all_students:
+    std_all.append(std)
+    
 @app.route("/")
 def Greet():
     return "<p>Welcome to Students Management API</p>"
 
 @app.route("/students",methods=["GET"])
 @basic_auth.required
-def get_all_books():
-    for std in all_students:
-        stdd.append(std)
-    return jsonify({"books":stdd})
+def get_all_std():
+    return jsonify({"books":std_all})
 
-@app.route("/books/<int:book_id>",methods=["GET"])
-def get_book(book_id):
-    book =  next(( b for b in books if b["id"]==book_id ),None)
-    if book:
-        return jsonify(book)
+@app.route("/students/<int:std_id>",methods=["GET"])
+@basic_auth.required
+def get_std(std_id):
+    for g in std_all:
+        std_id = str(std_id)
+        if  g["_id"] == std_id:
+            std = g
+            break
+        else:
+            std = None
+    if std:
+        return jsonify(std)
     else:
-        return jsonify({"error":"Book not found"}),404
+        return jsonify({"error":"Student not found"}),404
 
-@app.route("/books",methods=["POST"])
-def create_book():
+@app.route("/students",methods=["POST"])
+@basic_auth.required
+def create_std():
     data = request.get_json()
     new_book={
         "id":len(books)+1,
@@ -52,7 +61,8 @@ def create_book():
     return jsonify(new_book),201
 
 @app.route("/books/<int:book_id>",methods=["PUT"])
-def update_book(book_id):
+@basic_auth.required
+def update_std(book_id):
     book = next((b for b in books if b["id"]==book_id),None)
     if book:
         data = request.get_json()
@@ -65,7 +75,8 @@ def update_book(book_id):
 
 
 @app.route("/books/<int:book_id>",methods=["DELETE"])
-def delete_book(book_id):
+@basic_auth.required
+def delete_std(book_id):
     book = next((b for b in books if b["id"]==book_id),None)
     if book:
         books.remove(book)
